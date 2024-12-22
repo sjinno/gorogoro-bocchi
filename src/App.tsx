77cce46a592225c99ca2dcf1ee64e4aed4891617
Shohei Jinno <shohei.jinno@gmail.com>
2024-12-22 14:36:07 -0800

    use tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import gorogoroBocchi from "./assets/bocchi-the-rock-bocchi-the-rock-gif.gif";
+import "./index.css";
+import browser from "webextension-polyfill";
+
+const bocchiUrl = browser.runtime.getURL(
+  "bocchi-the-rock-bocchi-the-rock-gif.gif"
+);
 
 function App() {
   return (
-    <div style={{ position: "fixed", top: "0", left: "0" }}>
-      <img src={gorogoroBocchi} />
-    </div>
+    <div
+      className="absolute inset-0 w-full h-full flex justify-center items-center bg-repeat bg-[length:auto] bg-center"
+      style={{
+        backgroundImage: `url(${bocchiUrl})`,
+        backgroundSize: "auto", // Maintains aspect ratio
+      }}
+    ></div>
   );
 }
 
--- src/content.tsx
+++ src/content.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import browser from "webextension-polyfill";
 
 export function injectReact(elt: JSX.Element) {
   // Create the Shadow DOM and attach it to the page
@@ -8,11 +9,43 @@ export function injectReact(elt: JSX.Element) {
   const shadowRoot = container.attachShadow({ mode: "open" });
   document.body.appendChild(container);
 
+  // Inject Tailwind CSS
+  injectTailwindCSS(shadowRoot);
+
   // Mount the React App inside the Shadow DOM
   const shadowRootContainer = document.createElement("div");
+
+  {
+    shadowRootContainer.style.position = "fixed";
+    shadowRootContainer.style.top = "0";
+    shadowRootContainer.style.left = "0";
+    shadowRootContainer.style.width = "100%";
+    shadowRootContainer.style.height = "100%";
+  }
+
   shadowRoot.appendChild(shadowRootContainer);
   const root = createRoot(shadowRootContainer);
   root.render(elt); // Render react component
+}
+
+function injectTailwindCSS(shadowRoot: ShadowRoot) {
+  // Fetch the Tailwind CSS file
+  fetch(browser.runtime.getURL("style.css"))
+    .then((response) => response.text()) // Read the content as text
+    .then((css) => {
+      // Replace :root with :host for Shadow DOM scoping
+      const modifiedCSS = css.replace(/:root/g, ":host");
+
+      // Create a <style> element
+      const styleElement = document.createElement("style");
+      styleElement.textContent = modifiedCSS;
+
+      // Append the modified styles to the Shadow DOM
+      shadowRoot.appendChild(styleElement);
+    })
+    .catch((error) => {
+      console.error("Failed to load or modify Tailwind CSS:", error);
+    });
 }
 
 injectReact(<App />);
